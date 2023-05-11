d3df8d13c3e51a1195d7063fd3a349de73f33f34
hasitha_c <hasitha_c@epiclanka.net>
2023-05-11 21:45:57 +0530

    feat: bottom tab navigation

--- app/navigation/NavigationStack.tsx
+++ app/navigation/NavigationStack.tsx
@@ -2,18 +2,20 @@ import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import ProfileScreen from '../features/ProfileView/screens/ProfileScreen';
 import DummyScreen from '../features/ProfileView/screens/DummyScreen';
+import DashboardScreen from '../features/Dashboard/screens/DashboardScreen';
 
 const Stack = createStackNavigator();
 
 const NavigationStack = (): React.JSX.Element => {
   return (
     <Stack.Navigator
-      initialRouteName="ProfileScreen"
+      initialRouteName="DashboardScreen"
       screenOptions={{
         headerShown: false,
       }}>
       <Stack.Screen name="ProfileScreen" component={ProfileScreen} />
       <Stack.Screen name="DummyScreen" component={DummyScreen} />
+      <Stack.Screen name="DashboardScreen" component={DashboardScreen} />
     </Stack.Navigator>
   );
 };

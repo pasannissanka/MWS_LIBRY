b9d2519826ec50dd6549a3bc90fa30343d93aa5e
hasitha_c <hasitha_c@epiclanka.net>
2023-06-09 22:54:14 +0530

    feat: send reset password screen

--- app/navigation/NavigationStack.tsx
+++ app/navigation/NavigationStack.tsx
@@ -5,13 +5,14 @@ import DummyScreen from '../features/ProfileView/screens/DummyScreen';
 import DashboardScreen from '../features/Dashboard/screens/DashboardScreen';
 import OpeningScreen from '../features/AppAccess/screens/OpeningScreen';
 import LoginScreen from '../features/AppAccess/screens/LoginScreen';
+import SendResetPasswordScreen from '../features/AppAccess/screens/SendResetPasswordScreen';
 
 const Stack = createStackNavigator();
 
 const NavigationStack = (): React.JSX.Element => {
   return (
     <Stack.Navigator
-      initialRouteName="OpeningScreen"
+      initialRouteName="SendResetPasswordScreen"
       screenOptions={{
         headerShown: false,
       }}>
@@ -20,6 +21,10 @@ const NavigationStack = (): React.JSX.Element => {
       <Stack.Screen name="DashboardScreen" component={DashboardScreen} />
       <Stack.Screen name="OpeningScreen" component={OpeningScreen} />
       <Stack.Screen name="LoginScreen" component={LoginScreen} />
+      <Stack.Screen
+        name="SendResetPasswordScreen"
+        component={SendResetPasswordScreen}
+      />
     </Stack.Navigator>
   );
 };
